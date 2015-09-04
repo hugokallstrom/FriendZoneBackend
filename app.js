4e9f55380fe8237381d6df1c226f7ddc4df30e3b
oi11hkm <oi11hkm@cs.umu.se>
2015-09-04 21:06:34 +0200

    facebook login

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var restify = require('restify');
 var passport = require('passport');
-
+var auth = require('./lib/auth.js');
 var server = restify.createServer({
 	name: 'FriendZoneBackend',
 });
@@ -8,6 +8,13 @@ var server = restify.createServer({
 server.use(passport.initialize());
 server.use(restify.bodyParser());
 server.use(restify.authorizationParser());
+
+function respond(req, res, next) {
+		  res.send('hello ' + req.params.name);
+		    next();
+}
+
+server.post('/login/facebook', auth.isAuthenticated, respond);
 
 exports.start = function () {
 	server.listen(process.env.PORT || 8080, function() {
